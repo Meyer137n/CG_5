6fe06f9f8d22c6b535ec21455097a1fc75fcf983
Meyer137n <112759280+Meyer137n@users.noreply.github.com>
2024-11-18 17:35:32 +0700

    Реализация всех фильтров
    Фильтры: инверсия, бинаризация, оттенки серого, яркость, контрастность.
    Осталось добавить гистограмму

---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComputerGraphics_Filters;
+
+namespace ComputerGraphics_Filters
+{
+    public partial class Form1 : Form
+    {
+        Bitmap previous_image = null;
+        Bitmap image = null;
+        Filter lastFilter = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Файл
+
+        private void Open_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Открытие исходного изображения:";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                previous_image = image;
+                image = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = image;
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void Save_as_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Сохранение результата:";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                image.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        // Отмена
+
+        private void Cancel_button_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+        }
+
+        // Правка
+
+        private void Undo_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            image = previous_image;
+            pictureBox1.Image = image;
+            pictureBox1.Refresh();
+        }
+
+        private void Repeat_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StartFilter(lastFilter);
+        }
+
+        // BackgroundWorker1
+
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //Bitmap resultImage = ((Filter)e.Argument).processImage(image, backgroundWorker1);
+            Bitmap resultImage = ((Filter)e.Argument).processImage(new Bitmap(image), backgroundWorker1);
+
+            if (!backgroundWorker1.CancellationPending)
+            {
+                previous_image = image;
+                lastFilter = (Filter)e.Argument;
+                image = resultImage;
+            }
+        }
+
+        private void BackgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (!e.Cancelled)
+            {
+                pictureBox1.Image = image;
+                pictureBox1.Refresh();
+            }
+            progressBar1.Value = 0;
+        }
+
+        private void StartFilter(Filter filter)
+        {
+            if (backgroundWorker1.IsBusy == false)
+                backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void Inverse_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StartFilter(new InvertFilter());
+        }
+
+        private void GrayScale_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StartFilter(new GrayScaleFilter());
+        }
+        
+        private void Binarization_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int threshold = Convert.ToInt32(amountTextBox.Text); // Пороговое значение для бинаризации
+            StartFilter(new BinarizationFilter(threshold));
+        }
+
+        private void IncreaseBrightness_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int amount = Convert.ToInt32(amountTextBox.Text);
+            StartFilter(new BrightnessFilter(amount));
+        }
+
+        private void DecreaseBrightness_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int amount = Convert.ToInt32(amountTextBox.Text);
+            StartFilter(new BrightnessFilter(-amount));
+        }
+        
+        private void IncreaseContrast_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            double amount = Convert.ToDouble(amountTextBox.Text);
+            StartFilter(new ContrastFilter(amount));
+        }
+
+        private void DecreaseContrast_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           double amount = Convert.ToDouble(amountTextBox.Text);
+            StartFilter(new ContrastFilter(1/amount));
+        }
+    }
+}
